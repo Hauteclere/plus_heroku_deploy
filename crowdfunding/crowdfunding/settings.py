8df538ffb18b7e2fbef29450f178935a6b05f651
Oliver Lavers <oliver@shecodes.com.au>
2024-10-16 23:10:41 +1000

    updated config

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
-config = dotenv_values("../.env")
+load_dotenv("../.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,15 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get(
+SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY',
     'django-insecure-6f5fiv53l$d=%d_0_8&znvd!6&d3rfy-qowzswx^u)i-p_dsm6'
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config.get(
-    'DJANGO_DEBUG',
-    'True'
+DEBUG = os.environ.get(
+    'DJANGO_DEBUG'
 ) != 'False'
 
 ALLOWED_HOSTS = ['*']
